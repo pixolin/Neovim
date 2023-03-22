6de69c32c654dd2d508f07e8343f34e55ef42da1
Bego Mario Garde <pixolin@pixolin.de>
2023-03-22 12:05:10 +0100

    initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,18 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'windwp/nvim-autopairs' -- Autopair brackets, quotes…
+end)
