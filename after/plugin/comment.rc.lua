793ba57dce8a0dbe10ba2d5394d49cd6a29f0a98
Bego Mario Garde <pixolin@pixolin.de>
2023-03-24 13:18:41 +0100

    fix: typo in comment.rc.lua

--- after/plugin/comment.rc.lua
+++ after/plugin/comment.rc.lua
@@ -1,6 +1,6 @@
-local ok, Comment = pcall(require, 'Comment')
+local ok, todocomment = pcall(require, 'Comment')
 if not ok then
   print("Comment.nvim not loaded")
 end
 
-comment.setup()
+todocomment.setup()
